72452c4ce41ba787647362d5224bd908b0531b99
atilm <thielmann.andreas@googlemail.com>
2021-01-13 21:55:12 +0100

    prototype for world

--- game_of_life/src/App.vue
+++ game_of_life/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +16,17 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background-color: hsl(240, 5%, 75%);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
   margin-top: 60px;
 }
 </style>
